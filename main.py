a644ab4854f07215465680dc8eba3674ed1e0ae2
Szymonmikla <szymonmikla@gmail.com>
2023-01-03 19:57:42 +0100

    Main, requirements and VT_env files

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import whisper
 import ffmpeg
 
-model = whisper.load_model("large")
+model = whisper.load_model("tiny")
 
+def transcribe(audio):
+
+    transcription = model.transcribe(audio)
+
+    return transcription["text"]
